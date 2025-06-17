edef7e17d407a63273493e66efbc6f83435bfae9
Комаров Егор <123303256+pojalustayuidi@users.noreply.github.com>
2025-06-17 18:21:39 +0300

    test

--- routes/levels.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const levelController = require('../controllers/levelController');
-
-router.get('/', levelController.getAllLevels);
-router.get('/:id', levelController.getLevelById);
-router.post('/', levelController.addLevel);
-
-module.exports = router;
